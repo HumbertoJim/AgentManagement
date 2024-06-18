44e5e9dcd7fa709727271ea1925ae2a95b1ee281
HumbertoJim <humbertodjimz@gmail.com>
2024-06-17 22:44:08 -0500

    Migrate UniVRM v0.116.0 to v0.122.0

--- Assets/VRMShaders/GLTF/IO/Runtime/Texture/Importer/UnityTextureDeserializer.cs
+++ Assets/VRMShaders/GLTF/IO/Runtime/Texture/Importer/UnityTextureDeserializer.cs
@@ -1,21 +1,29 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using UnityEngine;
 
 namespace VRMShaders
 {
     /// <summary>
-    /// Unity の ImageConversion.LoadImage を用いて PNG/JPG の読み込みを実現する
+    /// Runtime での Texture2D 生成を実現するデフォルトの実装
     /// </summary>
     public sealed class UnityTextureDeserializer : ITextureDeserializer
     {
+        private readonly UnitySupportedImageTypeDeserializer _unitySupportedDeserializer = new();
+        private readonly KtxTextureDeserializer _ktxTextureDeserializer = new();
+
         public async Task<Texture2D> LoadTextureAsync(DeserializingTextureInfo textureInfo, IAwaitCaller awaitCaller)
         {
+            Texture2D texture = null;
             switch (textureInfo.DataMimeType)
             {
                 case "image/png":
+                    texture = await _unitySupportedDeserializer.LoadTextureAsync(textureInfo, awaitCaller);
                     break;
                 case "image/jpeg":
+                    texture = await _unitySupportedDeserializer.LoadTextureAsync(textureInfo, awaitCaller);
+                    break;
+                case "image/ktx2":
+                    texture = await _ktxTextureDeserializer.LoadTextureAsync(textureInfo, awaitCaller);
                     break;
                 default:
                     if (string.IsNullOrEmpty(textureInfo.DataMimeType))
@@ -29,14 +37,10 @@ namespace VRMShaders
                     break;
             }
 
-            var texture = new Texture2D(2, 2, TextureFormat.ARGB32, textureInfo.UseMipmap, textureInfo.ColorSpace == ColorSpace.Linear);
-            if (textureInfo.ImageData != null)
+            if (texture == null)
             {
-                texture.LoadImage(textureInfo.ImageData);
-                texture.wrapModeU = textureInfo.WrapModeU;
-                texture.wrapModeV = textureInfo.WrapModeV;
-                texture.filterMode = textureInfo.FilterMode;
-                await awaitCaller.NextFrame();
+                Debug.Log($"Failed to load texture from image data.");
+                texture = new Texture2D(2, 2, TextureFormat.ARGB32, textureInfo.UseMipmap, textureInfo.ColorSpace == ColorSpace.Linear);
             }
 
             return texture;
